6f4e3ad3b4b4d8cc1ba2bdce912c3fd8af734cf7
Владислав Буланкин <schirif@mail.ru>
2026-04-18 19:44:40 +0300

    1

--- src/BlackVPN.Common.Models/obj/Debug/net8.0/BlackVPN.Common.Models.AssemblyInfo.cs
+++ src/BlackVPN.Common.Models/obj/Debug/net8.0/BlackVPN.Common.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackVPN.Common.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77ebb295dbe362dde34e81b51d38f1b7ee605218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09001e0944931440c813a5c7351cc02cda9b3de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackVPN.Common.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackVPN.Common.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
